24e7fc39655f59e7f0f4461b5f3c16e5441e4a0a
vdavidguerrero2 <vincentdguerrero@gmail.com>
2018-06-10 02:21:49 +0000

    add, and d show

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,29 @@
+class ArticlesController < ApplicationController
+    
+    def new 
+        @article = Article.new
+    end
+    
+    def create 
+        @article = Article.new(article_params)
+        if @article.save 
+            flash[:notice] = "Se creo el articulo,manito!"
+            redirect_to article_path(@article)
+        else 
+            
+            render 'new'
+        end
+    end
+    
+    def show
+       @article = Article.find(params[:id])
+        
+    end
+    
+    private 
+    
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+    
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
 
-  root  'page#home'
-  get 'about', to: 'page#about'
+  root  'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
 
 
   # Example of regular route:
